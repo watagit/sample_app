ba53550497a49912ff70db9c05912944fc396c8b
Wataru Ono <wtr0414@gmail.com>
2019-11-27 14:32:33 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  root 'application#hello'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 end
