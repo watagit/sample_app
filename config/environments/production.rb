3477b2249c949833b98f6956145313dcf4e87597
Wataru Ono <wtr0414@gmail.com>
2019-12-08 02:02:46 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,20 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'floating-temple-09785.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
